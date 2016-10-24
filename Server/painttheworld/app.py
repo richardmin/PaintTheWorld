949ab73ba1b2bfc81c844323aac92ba624b6aaf6
Rodrigo Valle <rdan.valle@gmail.com>
2016-10-23 18:05:43 -0700

    Added debug page at '/debug'.
    The debug page still needs to be populated with information.

--- Server/painttheworld/app.py
+++ Server/painttheworld/app.py
@@ -5,7 +5,11 @@
 
 __all__ = [ 'app' ]
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.config.from_object('config')
+
+@app.route('/debug')
+def debug():
+    return render_template('debug.html')
