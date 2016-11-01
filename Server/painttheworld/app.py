8eb0be484596ca191a67047fd840db0d7c0ea2a7
Richard Min <richardmin97@gmail.com>
2016-11-01 03:37:14 -0700

    Endpoint templates Initialized

--- Server/painttheworld/app.py
+++ Server/painttheworld/app.py
@@ -5,7 +5,8 @@
 
 __all__ = [ 'app' ]
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import game
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -13,3 +14,17 @@ app.config.from_object('config')
 @app.route('/debug')
 def debug():
     return render_template('debug.html')
+
+@app.route('/game_data', methods=['POST'])
+def game_data():
+    # TODO: return actual JSON object
+    # request.form['location']
+    # request.form['magic']
+    return '{fake json object, with updated data}'
+
+# TODO: Support multiple lobbies
+@app.route('/join_lobby', methods=['GET', 'POST'])
+def join_lobby():
+    if request.method == 'POST'
+        user_count++; 
+    return '{users: \'{0}\', game_start_time: \'{1}\'}'.format(user_count, start_time)
