b61ba6f53857d62e69c18268895687365835bb9a
Richard Min <richardmin97@gmail.com>
2016-11-18 00:13:27 -0800

    Comments update, Bug Fixes, Help Functions
    * Endpoints.md updated to be up to date
    * No longer using globals for user count
    * Validation of longitude and latitude added
    * game_data input validation added
    * join_lobby latitude validation added
    * Comments added to functions in game.py
    * Removal of unnecessary getters/setters
    * Bug fixes on Longitude to Latitude converters

--- Server/painttheworld/app.py
+++ Server/painttheworld/app.py
@@ -7,6 +7,7 @@ __all__ = [ 'app' ]
 
 from flask import Flask, render_template, request
 from painttheworld.game import GameState
+import painttheworld.game
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -14,8 +15,13 @@ app.config.from_object('config')
 radius = 50 #2500 tiles
 gridsize = 5 #5 feet per tile. 
 
-
 active_game = None
+
+def validate_longitude(longitude):
+    return longitude >= -180 and longitude <= 180
+def validate_latitude(latitude):
+    return latitude >= -90 and latitude <= 90
+
 @app.route('/debug')
 def debug():
     return render_template('debug.html')
@@ -23,34 +29,47 @@ def debug():
 @app.route('/game_data', methods=['POST'])
 def game_data():
     global active_game
-    userid = request.form['user-id']
+    if 'user-id' not in request.form.to_dict() or 'long' not in request.form.to_dict() or 'lat' not in request.form.to_dict():
+        return '{error: \'data format invalid\'}'
+    if active_game is None or active_game.start_time is None:
+        return '{error: \'No Game In Progress\'}'
+    if request.form['id'] < 0 or request.form['id'] >= game.lobby_size:
+        return '{error: \'Invalid user id\'}'
 
-    # request.form['location']
-    # request.form['magic'] 
-    return '{{fake json object, with updated data}}'
+    if not validate_latitude(request.form['lat']):
+        return '{error: \'Invalid latitude degree\'}'
+    if not validate_longitude(request.form['long']):
+        return '{error: \'Invalid longitude degree\'}'
+    active_game.update_user(request.form['id'], request.form['long'], request.form['lat'])
+
+
+    return '{{}}'
 
 # TODO: Support multiple lobbies, probably in own file later
 @app.route('/join_lobby', methods=['GET', 'POST'])
 def join_lobby():
     global active_game, radius, gridsize
     if request.method == 'POST':
-        
         if 'lat' not in request.form.to_dict():
             return '{error: \'lat field not found\'}'
-        if 'long' not in request.form.to_dict():
+        if 'long' not in request.form.to_dict(): 
             return '{error: \'long field not found\'}'
+        if not validate_latitude(request.form['lat']):
+            return '{error: \'Invalid latitude degree\'}'
+        if not validate_longitude(request.form['long']):
+            return '{error: \'Invalid longitude degree\'}'
         if active_game is None:
             active_game = GameState(radius, gridsize) 
          
         usernum = active_game.add_user(request.form['lat'], request.form['long'])
         
-        if usernum is 8:
-            return '{{user-id: {0}, user-count: {1}, game_start_time: {3}}}'.format(usernum, active_game.get_user_count(), active_game.get_start_time())
-        else:    
-            return '{{user-id: {0}, user-count: {1}}}'.format(usernum, active_game.get_user_count())
+        if active_game.user_count is game.lobby_size:
+            return '{{user-id: {0}, user-count: {1}, game_start_time: {2}}}'.format(usernum, active_game.user_count, active_game.start_time.isoformat())
+        else:
+            return '{{user-id: {0}, user-count: {1}}}'.format(usernum, active_game.user_count)
         
     if active_game is None:
         return '{error: \'No game in progress\'}'
-    elif active_game.get_user_count() < 8:
-        return '{{user-count: {0}}}'.format(active_game.get_user_count())
-    return '{{user-count: {0}, game_start_time: {1}}}'.format(active_game.get_user_count(), active_game.get_start_time())
+    elif active_game.user_count < game.lobby_size:
+        return '{{user-count: {0}}}'.format(active_game.user_count)
+    return '{{user-count: {0}, game_start_time: {1}}}'.format(active_game.user_count, active_game.start_time.isoformat())
