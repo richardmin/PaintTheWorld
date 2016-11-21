f0e33bdc925357c80fbfaed13de4c42882522118
Rodrigo Valle <rdan.valle@gmail.com>
2016-11-20 20:15:01 -0800

    Removed some unused files.

--- Server/painttheworld/schema.sql
+++
@@ -1,9 +0,0 @@
-drop table if exists users;
-drop table if exists reviews;
-create table users (
-    id integer primary key autoincrement,
-    wins integer DEFAULT 0,
-    losses integer DEFAULT 0,
-    email varchar(255),
-    Google_OAuth_Token varchar(512)
-);
