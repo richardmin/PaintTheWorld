3cc4ccc28214c029372253bd042b2a1df361de85
Rodrigo Valle <rdan.valle@gmail.com>
2016-11-21 00:33:07 -0800

    Squeeze in a deploy.sh change.

--- Server/deploy.sh
+++ Server/deploy.sh
@@ -2,7 +2,7 @@
 set -e
 
 source venv/bin/activate
-branch=$1
+branch=${1:-master}
 
 git fetch
 git checkout $branch
@@ -10,3 +10,5 @@ git pull origin $branch
 
 pip install -r requirements.txt
 sudo systemctl restart gunicorn
+clear
+systemctl status gunicorn
