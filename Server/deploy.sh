114cf692b3eed1a0041640c01abb8934c12edc8e
Rodrigo Valle <rdan.valle@gmail.com>
2016-11-20 22:10:55 -0800

    Updated deploy.sh to take branch as argument.

--- Server/deploy.sh
+++ Server/deploy.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+set -e
 
 source venv/bin/activate
-git checkout server
-git pull origin server
+branch=$1
+
+git fetch
+git checkout $branch
+git pull origin $branch
+
 pip install -r requirements.txt
 sudo systemctl restart gunicorn
